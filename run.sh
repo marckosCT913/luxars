d2507fea0db73bd38aa951a5399a72370b0afdaa
Marckos666 <MPO33639411@soy.sena.edu.co>
2026-08-03 10:01:07 -0500

    Hacer run.sh robusto: PYTHONPATH explicito para que Django funcione aunque el shell desactive el user-site

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-# Arranca el servidor de LuxArs (Django instalado globalmente)
+# Arranca el servidor de LuxArs con el Python global.
+# PYTHONPATH explicito para que Django funcione aunque el shell desactive el user-site.
 cd "$(dirname "$0")"
+export PYTHONPATH="$PYTHONPATH:/home/jesusserpa/.local/lib/python3.14/site-packages"
 python manage.py runserver 127.0.0.1:8000
